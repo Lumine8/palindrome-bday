0127bd08a5ef6811cd37636831b7f9b2ffc48d6f
Lumine8 <sankargopan1@gmail.com>
2022-02-08 18:55:56 +0530

    update: started working

---
+++ app.js
@@ -0,0 +1,5 @@
+var date = document.querySelector("#date")
+var output = document.querySelector("#output")
+var button = document.querySelector("#btn")
+
+button.addEventListener("click", function )
