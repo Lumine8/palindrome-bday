a63f029ee0d162dc2d4c922bd1b30cadbcb35c8c
Lumine8 <sankargopan1@gmail.com>
2022-02-08 21:20:38 +0530

    update:website complete

--- app.js
+++ app.js
@@ -2,4 +2,28 @@ var date = document.querySelector("#date")
 var output = document.querySelector("#output")
 var button = document.querySelector("#btn")
 
-button.addEventListener("click", function )
+button.addEventListener("click", ispalindrome)
+
+function ispalindrome(){
+    var arrayValue1 = date.value
+
+    var arrayValue = ""
+    for(var i = 0; i < arrayValue1.length; i++){
+        if(i >= 0 && i<=1){
+            continue;
+        }
+        else{
+            arrayValue+=arrayValue1[i].toString();
+        }
+    }
+
+    var arrayValue2 = arrayValue.split('')
+    var reverseArrayValue = arrayValue2.reverse();
+    var reverseString = reverseArrayValue.join('');
+    if (arrayValue==reverseString){
+        output.innerText = "Your birthday is a palindrome"
+    }
+    else{
+        output.innerText = "Your birthday is not a palindrome"
+    }
+}
